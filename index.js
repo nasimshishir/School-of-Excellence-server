eb91d71001081f9d661d9c81a57de2c6044522c6
nasimshishir <tech17196@gmail.com>
2022-10-26 15:19:56 +0600

    Data arranged for JSON file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const courses = require('./data/courses.json');
 
 app.get("/", (req, res) => {
     res.send('Server side Running')
+})
+
+app.get("/courses", (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
