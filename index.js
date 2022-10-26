d82a008ee4f0cd82abee92b9c98b2c74d65b1789
nasimshishir <tech17196@gmail.com>
2022-10-27 05:19:21 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get("/courses", (req, res) => {
     res.send(courses);
 })
 
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const singleCourse = courses.find(course => course.id === id);
+    res.send(singleCourse);
+})
+
 app.listen(port, () => {
     console.log('Education Server running on port- ', port);
 })
